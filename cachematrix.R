63748e307780e3c1a8ebbb3a3506ec6d11879283
Lorae Stojanovic <stojanovic.lorae@gmail.com>
2015-06-03 12:51:10 -0400

    i This is my submission.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions aid in creating the inverse of a large matrix. makeCacheMatrix
+## create a list that contains information that be used to check if a matrix inverse is cached 
+## and cacheSolve will check that list to see if there is a cached inverse. If so, it will retrieve it.
+## If not, i will generate a new one.
 
-## Write a short comment describing this function
+## Creates a list that can be used to check if a matrix inverse is cached and retrieve it usign the
+## cacheSolve function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # initializing m
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    # nothing is cached yet
+    m <<- NULL
+  }
+  # used in other function to retrieve the unaltered matrix
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  # the list that provides variables needed for cacheSolve
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Checks if inverse already exists; if not, generates a new one
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # retrieving m
+  m <- x$getinv()
+  # checking if m is cached
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  # calculating inverse
+  m <- solve(data, ...)
+  # altering the "setinv" entry in list from makeCacheMatrix
+  x$setinv(m)
+  m
 }
